d5fc6a8232a9080f7f7be35a36b492fa6535bf6f
phpdie <sundaysunsun@vip.qq.com>
2022-03-28 11:39:32 +0800

    更改

--- app/common.php
+++ app/common.php
@@ -1,2 +1,31 @@
 <?php
 // 应用公共文件
+
+if (!function_exists('blank')) {
+    /**
+     * Determine if the given value is "blank".
+     *
+     * @param mixed $value
+     * @return bool
+     */
+    function blank($value)
+    {
+        if (is_null($value)) {
+            return true;
+        }
+
+        if (is_string($value)) {
+            return trim($value) === '';
+        }
+
+        if (is_numeric($value) || is_bool($value)) {
+            return false;
+        }
+
+        if ($value instanceof Countable) {
+            return count($value) === 0;
+        }
+
+        return empty($value);
+    }
+}

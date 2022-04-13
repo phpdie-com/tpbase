688bbf3a3f387c9a3c02c46637c4ccb4394c870d
phpdie <sundaysunsun@vip.qq.com>
2022-04-13 17:39:44 +0800

    添加filp/whoops 组件

--- app/ExceptionHandle.php
+++ app/ExceptionHandle.php
@@ -52,7 +52,22 @@ class ExceptionHandle extends Handle
     public function render($request, Throwable $e): Response
     {
         // 添加自定义异常处理机制
+        if (ENV('APP_DEBUG')) {
+            // 如果是HttpResponseException异常则原样输出
+            // JUMP插件里的success,error和result方法均返回的是HttpResponseException异常
+            if ($e instanceof HttpResponseException) {
+                return $e->getResponse();
+            }
 
+            // Whoops 接管请求异常
+            $whoops = new \Whoops\Run;
+            $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
+            return Response::create(
+                $whoops->handleException($e),
+                'html',
+                500
+            );
+        }
         // 其他错误交给系统处理
         return parent::render($request, $e);
     }
